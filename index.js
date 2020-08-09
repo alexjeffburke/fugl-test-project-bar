alexjeffburke/fugl-test-project

Initial commit.

index.js
@@ -1,0 +1,3 @@
+const foo = require('dont-break-foo');
+
+process.exit(foo === 'foo' ? 0 : 1);

alexjeffburke/fugl-test-project-bar

index.js
@@ -1,0 +1,3 @@
+const foo = require('dont-break-foo');
+
+process.exit(foo === 'bar' ? 0 : 1);